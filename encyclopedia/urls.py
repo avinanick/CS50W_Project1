47b65add061cf43e582e0953fa3ade41d4c87754
Nick Avina <avinanick@gmail.com>
2020-09-05 12:30:04 -0700

    I previously had the edit page linked to the new page, fixed that and added the title variable.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/search", views.search, name="search"),
     path("wiki/new_page", views.new_page, name="new_page"),
-    path("wiki/edit_page", views.new_page, name="edit_page"),
+    path("wiki/edit_page/<str:title>", views.edit_page, name="edit_page"),
     path("wiki/<str:title>", views.entry, name="entry")
 ]
