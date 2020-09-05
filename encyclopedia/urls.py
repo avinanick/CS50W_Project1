b18709a296cd1ebc96540b4bbfaba0731360a446
Nick Avina <avinanick@gmail.com>
2020-09-04 22:34:03 -0700

    Fixed a problem with my url search in which it had parenthesis instead of brackets.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/search", views.search, name="search"),
-    path("wiki/new", views.new_page, name="new_page"),
+    path("wiki/new_page", views.new_page, name="new_page"),
     path("wiki/<str:title>", views.entry, name="entry")
 ]
